03b612ae8e2e7689e8bbc340bf22b61a6a3f033e
Alexander Böhm <tools@boehm.work>
2022-07-08 17:27:18 +0200

    chore: add jetbrain configs
    Also switch from `.cjs` extensions to `json` files.

--- scripts/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['../.eslintrc.cjs'],
-  rules: {
-    'no-console': 'off',
-    'unicorn/no-process-exit': 'off',
-  },
-};
